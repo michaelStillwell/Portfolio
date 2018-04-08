1b7540919e0a70e057cce6a91fcbec7837e83208
Michael <michael.stillwell@hotmail.com>
2018-04-08 15:59:28 -0500

    Test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 // React Imports
 import React, { Component } from 'react';
-
 // window.scrollTo(x,y) on click to scroll to a position
 import './App.css';
+
+import ReactLogo from './Assets/react-logo.png';
+import ReduxLogo from './Assets/ReduxLogo.png';
+import NodeLogo from './Assets/logo_js.svg';
+import PostLogo from './Assets/PostgreSQL.png';
+import ElectronLogo from './Assets/electron_logo.svg';
+import HTMLLogo from './Assets/html_logo.png';
+import CSSLogo from './Assets/css_logo.svg';
+import SassLogo from './Assets/sass_logo.png';
+import JSLogo from './Assets/javascript_logo.png';
+import GMLLogo from './Assets/gml_logo.png';
+
 
 // <div>
 
@@ -30,34 +41,47 @@ import './App.css';
 // </ul>
 
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            slide: 0,
+            logo: JSLogo
+        }
+    }
+
     render() {
+        let logo = HTMLLogo;
+        setTimeout(() => {
+            if ( this.state.slide >= 4 ) {
+                this.setState({ slide: 0 });
+            } else {
+                this.setState({ slide: this.state.slide + 1})
+            }
+        }, 1500);
+
         return (
             <div>
-                <input type="checkbox" id="toggle" />
-                <div id="side-bar">
-                    <div id="close" onClick={() => {
-                        console.log('hi');
-                    }}><strong>X</strong></div>
-                    <div id="project">
-                        <div className="tab" onClick={() => {
-                            document.getElementById('toggle').checked = !document.getElementById('toggle').checked;
-                        }}>PROJECTS</div>
-                        <img />
-                        <h3><strong>Order</strong></h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                        <a href="http://159.89.50.120/#/" 
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >Click here</a>
-                    </div>
-                    <label><strong>Technologies Used:</strong></label>
-                    <ul>
-                        <li>React.js</li>
-                        <li>Node.js</li>
-                        <li>Express.js</li>
-                        <li>Sass</li>
-                        <li>Gulp</li>
+                <div className="body">
+                    <h3><i>Jr. Full-Stack Web Developer</i></h3>
+                    <h1>Michael Stillwell</h1>
+                    {/* <img src={logo} className="slider" /> */}
+                    <ul id="slides">
+                        <li className="slide showing"><img src={JSLogo} alt="JavaScript"/></li>
+                        <li className="slide"><img src={HTMLLogo} alt="HTML"/></li>
+                        <li className="slide"><img src={CSSLogo} alt="CSS"/></li>
+                        <li className="slide"><img src={ReactLogo} alt="React"/></li>
                     </ul>
+                    {/* <img src={ReactLogo} alt="React"/>
+                    <img src={ReduxLogo} alt="Redux"/>
+                    <img src={NodeLogo} alt="Logo"/>
+                    <img src={PostLogo} alt="PostgreSQL"/>
+                    <img src={ElectronLogo} alt="Electron"/>
+                    <img src={HTMLLogo} alt="HTML"/>
+                    <img src={CSSLogo} alt="CSS"/>
+                    <img src={SassLogo} alt="Sass"/>
+                    <img src={JSLogo} alt="JavaScript"/>
+                    <img src={GMLLogo} alt="GML"/> */}
                 </div>
             </div>
         )
